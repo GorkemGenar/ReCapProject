b1a4aec83bb56c98c77b3eb2ce60eaeb66481ac0
GorkemGenar <gorkem_akkaya@hotmail.com>
2021-08-17 11:02:07 +0300

    .

--- Entities/DTOs/CarDto.cs
+++ Entities/DTOs/CarDto.cs
@@ -6,18 +6,11 @@ using System.Text;
 
 namespace Entities.DTOs
 {
-    public class CarDto : IDto
+    public class CarDto : Car, IDto
     {
-        public int CarId { get; set; }
-        public int BrandId { get; set; }
-        public int ColorId { get; set; }
         public int CarImageId { get; set; }
-        public string CarName { get; set; }
+        public List<CarImage> CarImage { get; set; }
         public string BrandName { get; set; }
         public string ColorName { get; set; }
-        public List<CarImage> CarImage { get; set; }
-        public int ModelYear { get; set; }
-        public decimal DailyPrice { get; set; }
-        public string Description { get; set; }
     }
 }
